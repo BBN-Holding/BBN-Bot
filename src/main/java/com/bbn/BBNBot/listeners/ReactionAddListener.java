dd7208045ad086baa3c85471b12c49eed1587787
GregTCLTK <gregtcltk@gmail.com>
2019-12-01 12:46:17 +0100

    .complete();

--- src/main/java/com/bbn/BBNBot/listeners/ReactionAddListener.java
+++ src/main/java/com/bbn/BBNBot/listeners/ReactionAddListener.java
@@ -43,6 +43,7 @@ public class ReactionAddListener extends ListenerAdapter {
                 event.getGuild().addRoleToMember(event.getMember(), event.getGuild().getRoleById("648518759971160089")).reason("Want a bot").queue();
                 event.getReaction().removeReaction(event.getUser()).queue();
                 TextChannel channel = event.getGuild().createTextChannel(event.getUser().getName()).setParent(event.getGuild().getCategoryById("648518640718839829")).complete();
+                channel.getManager().setTopic("Bot request by " + event.getUser().getAsTag() + " ID: " + event.getMember().getId()).reason("User wants a bot").complete();
                 channel.createPermissionOverride(event.getMember()).setAllow(
                         Permission.VIEW_CHANNEL,
                         Permission.MESSAGE_WRITE,
@@ -50,7 +51,6 @@ public class ReactionAddListener extends ListenerAdapter {
                         Permission.MESSAGE_ATTACH_FILES,
                         Permission.MESSAGE_EMBED_LINKS
                 ).reason("User wants a bot").queue();
-                channel.getManager().setTopic("Bot request by " + event.getUser().getAsTag() + " ID: " + event.getMember().getId()).reason("User wants a bot").queue();
                 event.getGuild().getTextChannelById("452789888945750046").sendMessage("<@477141528981012511> ").queue();
                 event.getGuild().getTextChannelById("452789888945750046").sendMessage(new EmbedBuilder()
                         .setTitle("User wants a bot")
