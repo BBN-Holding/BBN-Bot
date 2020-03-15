d4e89b3aa7356fd2a8cec4ab6cc1e9baf08fcc6e
GregTCLTK <gregtcltk@gmail.com>
2020-03-15 19:07:33 +0100

    Introduce json config system

--- src/main/java/com/bbn/bot/BBNBot.java
+++ src/main/java/com/bbn/bot/BBNBot.java
@@ -1,12 +1,12 @@
 package com.bbn.bot;
 
+import com.bbn.bot.core.Config;
 import com.bbn.bot.core.Sender;
 import com.bbn.bot.listeners.*;
 import net.dv8tion.jda.api.AccountType;
 import net.dv8tion.jda.api.JDABuilder;
 import net.dv8tion.jda.api.OnlineStatus;
 import net.dv8tion.jda.api.entities.Activity;
-import com.bbn.bot.util.*;
 
 /**
 * @author GregTCLTK / Skidder
@@ -17,10 +17,12 @@ public class BBNBot {
     public static void main(String[] args) {
 
         Sender sender = new Sender();
+        Config config = new Config("./BBN_config.json");
+        config.load();
 
         JDABuilder builder = new JDABuilder(AccountType.BOT);
         builder.setActivity(Activity.streaming("on the BBN", "https://twitch.tv/bigbotnetwork"))
-                .setToken(SECRETS.TOKEN)
+                .setToken(config.getToken())
                 .setAutoReconnect(true)
                 .setStatus(OnlineStatus.DO_NOT_DISTURB)
                 .addEventListeners(new MemberJoinListener(),
---
+++ src/main/java/com/bbn/bot/core/Config.java
@@ -0,0 +1,31 @@
+package com.bbn.bot.core;
+
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+public class Config {
+
+    private Path file;
+    private JSONObject config;
+
+    public Config(String path) {
+        this.file = Paths.get(path);
+    }
+
+    public void load() {
+        try {
+            config = new JSONObject(new String(Files.readAllBytes(file)));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public String getToken() {
+        return config.getString("Token");
+    }
+
+}
