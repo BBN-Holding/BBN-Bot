f38eac5732472b37f84c6d71357ebd5f3ea5a1ba
GregTCLTK <gregtcltk@gmail.com>
2020-03-16 16:32:17 +0100

    Finish voice logging

--- src/main/java/com/bbn/bot/listeners/VoiceLogListener.java
+++ src/main/java/com/bbn/bot/listeners/VoiceLogListener.java
@@ -41,21 +41,73 @@ public class VoiceLogListener extends ListenerAdapter {
 
     @Override
     public void onGuildVoiceMute(@Nonnull GuildVoiceMuteEvent event) {
+        TextChannel c = event.getJDA().getTextChannelById("689123407249670164");
+        if (event.getVoiceState().isMuted()) {
+            c.sendMessage(new EmbedBuilder()
+                    .setAuthor(event.getMember().getUser().getAsTag(), event.getMember().getUser().getAvatarUrl(), event.getMember().getUser().getAvatarUrl())
+                    .setTitle("User muted")
+                    .addField("Channel", event.getVoiceState().getChannel().getName(), true)
+                    .addField("Members in Channel", String.valueOf(event.getVoiceState().getChannel().getMembers().size()), true)
+                    .setColor(Color.RED)
+                    .setFooter("Provided by BBN", "https://bigbotnetwork.com/images/avatar.png")
+                    .setTimestamp(Instant.now())
+                    .build()).queue();
+        } else {
+            c.sendMessage(new EmbedBuilder()
+                    .setAuthor(event.getMember().getUser().getAsTag(), event.getMember().getUser().getAvatarUrl(), event.getMember().getUser().getAvatarUrl())
+                    .setTitle("User unmuted")
+                    .addField("Channel", event.getVoiceState().getChannel().getName(), true)
+                    .addField("Members in Channel", String.valueOf(event.getVoiceState().getChannel().getMembers().size()), true)
+                    .setColor(Color.GREEN)
+                    .setFooter("Provided by BBN", "https://bigbotnetwork.com/images/avatar.png")
+                    .setTimestamp(Instant.now())
+                    .build()).queue();
+        }
         super.onGuildVoiceMute(event);
     }
 
     @Override
     public void onGuildVoiceJoin(@Nonnull GuildVoiceJoinEvent event) {
+        TextChannel c = event.getJDA().getTextChannelById("689123407249670164");
+        c.sendMessage(new EmbedBuilder()
+                .setAuthor(event.getMember().getUser().getAsTag(), event.getMember().getUser().getAvatarUrl(), event.getMember().getUser().getAvatarUrl())
+                .setTitle("User joined")
+                .addField("Channel", event.getVoiceState().getChannel().getName(), true)
+                .addField("Members in Channel", String.valueOf(event.getVoiceState().getChannel().getMembers().size()), true)
+                .setColor(Color.GREEN)
+                .setFooter("Provided by BBN", "https://bigbotnetwork.com/images/avatar.png")
+                .setTimestamp(Instant.now())
+                .build()).queue();
         super.onGuildVoiceJoin(event);
     }
 
     @Override
     public void onGuildVoiceLeave(@Nonnull GuildVoiceLeaveEvent event) {
+        TextChannel c = event.getJDA().getTextChannelById("689123407249670164");
+        c.sendMessage(new EmbedBuilder()
+                .setAuthor(event.getMember().getUser().getAsTag(), event.getMember().getUser().getAvatarUrl(), event.getMember().getUser().getAvatarUrl())
+                .setTitle("User left")
+                .addField("Channel", event.getVoiceState().getChannel().getName(), true)
+                .addField("Members in Channel", String.valueOf(event.getVoiceState().getChannel().getMembers().size()), true)
+                .setColor(Color.RED)
+                .setFooter("Provided by BBN", "https://bigbotnetwork.com/images/avatar.png")
+                .setTimestamp(Instant.now())
+                .build()).queue();
         super.onGuildVoiceLeave(event);
     }
 
     @Override
     public void onGuildVoiceMove(@Nonnull GuildVoiceMoveEvent event) {
+        TextChannel c = event.getJDA().getTextChannelById("689123407249670164");
+        c.sendMessage(new EmbedBuilder()
+                .setAuthor(event.getMember().getUser().getAsTag(), event.getMember().getUser().getAvatarUrl(), event.getMember().getUser().getAvatarUrl())
+                .setTitle("User moved")
+                .addField("New Channel", event.getVoiceState().getChannel().getName(), true)
+                .addField("Members in Channel", String.valueOf(event.getVoiceState().getChannel().getMembers().size()), true)
+                .setColor(Color.YELLOW)
+                .setFooter("Provided by BBN", "https://bigbotnetwork.com/images/avatar.png")
+                .setTimestamp(Instant.now())
+                .build()).queue();
         super.onGuildVoiceMove(event);
     }
 }
