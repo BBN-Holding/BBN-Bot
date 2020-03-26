4693b4890aa081f97c267794b232ec16440bcb89
GregTCLTK <gregtcltk@gmail.com>
2020-03-26 11:09:42 +0100

    Replace deprecated stuff

--- src/main/java/com/bbn/bot/BBNBot.java
+++ src/main/java/com/bbn/bot/BBNBot.java
@@ -3,7 +3,6 @@ package com.bbn.bot;
 import com.bbn.bot.core.Config;
 import com.bbn.bot.core.Sender;
 import com.bbn.bot.listeners.*;
-import net.dv8tion.jda.api.AccountType;
 import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.JDABuilder;
 import net.dv8tion.jda.api.OnlineStatus;
@@ -23,9 +22,8 @@ public class BBNBot {
         Sender sender = new Sender();
         config.load();
 
-        JDABuilder builder = new JDABuilder(AccountType.BOT);
+        JDABuilder builder = JDABuilder.createDefault(config.getToken());
         builder.setActivity(Activity.streaming("on the BBN", "https://twitch.tv/bigbotnetwork"))
-                .setToken(config.getToken())
                 .setAutoReconnect(true)
                 .setStatus(OnlineStatus.DO_NOT_DISTURB)
                 .addEventListeners(new MemberJoinListener(),
