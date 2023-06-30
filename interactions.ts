8ff66e9733d7b1685c38d432c2216a6ca8a43290
Skidder <Skidder@bbn.one>
2023-06-30 11:04:17 +0200

    feat: ticket closing queue

--- interactions.ts
+++ interactions.ts
@@ -35,6 +35,11 @@ export async function handleInteraction(interaction: Interaction, db: DB) {
                 interaction.reply({
                     content: `> We're closing your ticket. Please be patient.`,
                 });
+
+                if (interaction.user.id != ("401817301919465482" || "261083609148948488")) {
+                    channel.setParent("1124263122895646802", { lockPermissions: false, reason: "Ticket closed by " + interaction.user.tag })
+                    break;
+                }
                 const messages = await channel.messages.fetch();
                 let member;
                 try {
@@ -229,7 +234,7 @@ export async function handleInteraction(interaction: Interaction, db: DB) {
             reason: "Ticket escalated",
         });
         interaction.reply({
-            allowedMentions: {roles: ['757969277063266407']},
+            allowedMentions: { roles: [ '757969277063266407' ] },
             content: "Ticket escalated. || <@&757969277063266407>"
         });
     }
